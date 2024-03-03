de6de88402685dbeed0946344822037f738239e9
Miguel Tafur <miguelmita03@gmail.com>
2024-03-03 16:04:30 -0300

    Alterando permiso de agregar base con el código 290

--- Controllers/Resumen.php
+++ Controllers/Resumen.php
@@ -162,7 +162,13 @@
 		{
 			if($_POST)
 			{
-				if($_SESSION['idUser'] == 1 || $_SESSION['idUser'] == 254 || $_SESSION['idUser'] == 1087 || $_SESSION['idUser'] == 219 || $_SESSION['idUser'] == 1205 || $_SESSION['idUser'] == 1289)
+				if($_SESSION['idUser'] == 1 
+					|| $_SESSION['idUser'] == 254 
+					|| $_SESSION['idUser'] == 1087 
+					|| $_SESSION['idUser'] == 219 
+					|| $_SESSION['idUser'] == 1205 
+					|| $_SESSION['idUser'] == 1289 
+					|| $_SESSION['idUser'] == 290)
 				{
 					$intBase = intval($_POST['txtBase']);
 					$strObservacion = strClean($_POST['txtObservacion']);
